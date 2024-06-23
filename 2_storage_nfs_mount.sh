55d66874aa52125c8150eb822ed82b3726d7ccd5
minyang <mychen76@gmail.com>
2024-06-23 23:39:33 +0000

    clean up

--- 2_storage_nfs_mount.sh
+++ 2_storage_nfs_mount.sh
@@ -5,6 +5,6 @@ echo "create mount point /mnt/workspace/..."
 
 sudo mkdir -p /mnt/workspace
 sudo chmod -R 775 /mnt/workspace
-sudo mount -t nfs4 localhost: /mnt/workspace
+sudo mount -t nfs4 nfs: /mnt/workspace
 df -h
 ls -all /mnt/workspace
